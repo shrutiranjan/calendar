b8e6ad65bee95564835bd9c5979cdfb711d18c62
Shrutiranjan <shrutiranjan.pattanayak@sc.com>
2017-12-13 06:12:12 +0000

    second teestcase complete

--- calendar-app/src/DatePicker.js
+++ calendar-app/src/DatePicker.js
@@ -4,47 +4,85 @@ import styled from 'styled-components'
 
 const DAYS_NAME = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT']
 export const MONTHS_NAME = ["January", "February", "March", "April", "May", "June",
-"July", "August", "September", "October", "November", "December"
-]
+"July", "August", "September", "October", "November", "December"]
 
 const DateCell = (props) => {
-    const {cellDate, selected, greyedOut, isActive} = props
+    const {cellDate, selected, greyedOut, isActive, onSelect} = props
     //console.log(cellDate)
+    const setDate = () => {
+        onSelect(cellDate)
+    }
     return (
-        <DateDiv selected={selected} greyedOut={greyedOut} isActive={isActive}>
+        <DateDiv selected={selected} greyedOut={greyedOut} isActive={isActive} onClick={setDate}>
             {cellDate.getDate()}
         </DateDiv>
     )
 }
 export default class DatePicker extends React.Component<Props, State> {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
             activeDate: new Date(),
-            selectedDate: new Date() 
+            selectedDate: new Date(),
+            viewMonth: null,
+            viewYear: null
         }
     }
-   
+    componentWillMount() {
+        this.setState({viewMonth: this.state.selectedDate.getMonth(), viewYear: this.state.selectedDate.getFullYear()})
+    }        
+    goPrev = () => {
+        if(this.state.viewMonth === 0) {
+            this.setState({viewMonth: 11, viewYear: this.state.viewYear - 1})
+        } else {
+            this.setState({viewMonth: this.state.viewMonth - 1})
+        }
+    }
+
+    goNext = () => {
+        if(this.state.viewMonth === 11) {
+            this.setState({viewMonth: 0, viewYear: this.state.viewYear + 1})
+        } else {
+            this.setState({viewMonth: this.state.viewMonth + 1})
+        }
+    }
+
+    selectDate = (dt) => {
+        this.setState({selectedDate: dt})
+    }
+
+    onCancelClick = () => {
+        console.log('cancel clicked')
+        if(this.props.onCancel) {
+            this.props.onCancel()
+        }
+    }
+    
+    onSaveClick = () => {
+        console.log('save clicked on date', this.state.selectedDate)
+        if(this.props.onSave) {
+            this.props.onSave(this.state.selectedDate)
+        }
+    }
+
     render = () => {
-        const {selectedDate, activeDate} = this.state
+        const {selectedDate, activeDate, viewMonth, viewYear} = this.state
         selectedDate.setHours(0,0,0,0)
-        activeDate.setHours(0,0,0,0)
-        const selectDay = selectedDate.getDate()
-        const selectMonth  = selectedDate.getMonth()
-        const selectYear = selectedDate.getFullYear()
-        const firstDate = new Date(selectYear, selectMonth, 1)
-        firstDate.setHours(0,0,0,0)
-        const lastDate = new Date(selectYear, selectMonth+1, 0)
+        activeDate.setHours(0,0,0,0)       
+        let firstDate = new Date(viewYear, viewMonth, 1)        
+        //firstDate.setHours(0,0,0,0)
+        //console.log(viewYear, viewMonth)
+        let lastDate = new Date(viewYear, viewMonth+1, 0)
         firstDate.setHours(0,0,0,0)
         let dayOfFirstIndex = -firstDate.getDay()
         
         return (
-            <table>
+            <Table>
                 <thead>
                 <tr>
-                    <th><button>&lt;</button></th>
-                    <th colSpan="5"><span id="selected_month">{MONTHS_NAME[selectMonth]}</span> <span id="selected_year">{selectYear}</span></th>
-                    <th><button>&gt;</button></th>
+                    <th><button onClick={this.goPrev}>&lt;</button></th>
+                    <th colSpan="5"><span id="selected_month">{MONTHS_NAME[viewMonth]}</span> <span id="selected_year">{viewYear}</span></th>
+                    <th><button onClick={this.goNext}>&gt;</button></th>
                 </tr>
                 <tr>
                 {DAYS_NAME.map(day => {
@@ -57,12 +95,12 @@ export default class DatePicker extends React.Component<Props, State> {
                     return (
                         <tr key={rowIndex}>
                             {Array.from({length: 7}).map((col, colIndex) => {
-                                    let cellDate = new Date()
+                                    let cellDate = new Date(viewYear, viewMonth, 1)
                                     cellDate.setDate(firstDate.getDate() + dayOfFirstIndex)
                                     cellDate.setHours(0,0,0,0)
                                     dayOfFirstIndex+=1
                                     //console.log(selectedDate.valueOf(),  cellDate.valueOf(), "======"+cellDate.getDate())
-                                    return (<td key={colIndex}><DateCell cellDate={cellDate} isActive={activeDate.getTime() === cellDate.getTime()} selected={selectedDate.getTime() === cellDate.getTime()} greyedOut={firstDate.getTime() > cellDate.getTime() || lastDate.getTime() < cellDate.getTime()} /></td>)
+                                    return (<td key={colIndex}><DateCell onSelect={this.selectDate} cellDate={cellDate} isActive={activeDate.getTime() === cellDate.getTime()} selected={selectedDate.getTime() === cellDate.getTime()} greyedOut={firstDate.getTime() > cellDate.getTime() || lastDate.getTime() < cellDate.getTime()} /></td>)
                                     
                                 })
                             }
@@ -70,17 +108,26 @@ export default class DatePicker extends React.Component<Props, State> {
                     )
                 })}
                 </tbody>
-            </table>
+                <tfoot>
+                    <tr>
+                    <th colSpan="5"></th>
+                    <td><button onClick={this.onCancelClick}>Cancel</button></td>
+                    <td><button onClick={this.onSaveClick}>Save</button></td>
+                    </tr>
+                </tfoot>
+            </Table>
         )
     }
 }
 
-
+const Table = styled.table `
+    margin: 0 auto;
+`
 export const DateDiv = styled.div`
     padding: 20px;
     background-color: ${props => (props.greyedOut ? '#AAA' : 'none')};
-    background-color: ${props => (props.selected ? '#00FFFF' : 'none')};
-    border: ${props => (props.isActive ? '1px solid #00FFFF' : '1px solid #333')}; ;
+    background-color: ${props => (props.selected ? '#FFC0CB' : 'none')};
+    border: ${props => (props.isActive ? '2px solid #FF0000' : '1px solid #999')}; ;
     font-weight: ${props => (props.selected ? 'bold' : 'normal')};
-
+    cursor: pointer;
 `
--- calendar-app/src/__tests__/DatePicker.test.js
+++ calendar-app/src/__tests__/DatePicker.test.js
@@ -8,7 +8,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { mount } from 'enzyme'
 //import App from './App'
-import DatePicker, {MONTHS_NAME} from '../DatePicker'
+import DatePicker, {MONTHS_NAME, DateDiv} from '../DatePicker'
 
 const fail = done => done.fail()
 
@@ -19,7 +19,7 @@ describe(`By default, when no props are passed`, () => {
   beforeEach(() => { 
     wrapper = mount(<DatePicker />)
     todayDate = new Date()
-    //todayDate.setHours(0,0,0,0)
+    todayDate.setHours(0,0,0,0)
   })
 
   it('should map the date to the correct day of the week', () => {
@@ -32,8 +32,6 @@ describe(`By default, when no props are passed`, () => {
   })
 
   it(`today's date is both selected & is active`, () => {
-    const firstDate = new Date(todayDate.getFullYear(), todayDate.getMonth(), 1)
-    const firstDayIndex = firstDate.getDay()
     const allTd = wrapper.find('tbody').find('td')
     expect(
       allTd.find('td').find({selected: true}).at(0).text()
@@ -55,18 +53,51 @@ describe(`By default, when no props are passed`, () => {
       ).toEqual(todayDate.getFullYear().toString())
     })
   })
-
-  // it('should apply different classes for dates displayed from prev & next month', fail)
-  // it('should fill the missing cells in the month in the grid with dates from previous & next months', fail)
-  // it(
-  //   'should apply the selected style on a newly selected date & isActive style should still be on the original date',
-  //   fail
-  // )
-
-  describe(`doesn't throw an error when`, () => {
-    it(`saved is clicked`, fail)
-    it(`cancel is clicked`, fail)
+  it('should apply different classes for dates displayed from prev & next month', () => {
+    const firstDate = new Date(todayDate.getFullYear(), todayDate.getMonth(), 1)
+    const firstDayIndex = firstDate.getDay()
+    if(firstDayIndex > 0) {
+      const firstRow = wrapper.find('tbody').find('tr').at(0)
+      expect(
+        firstRow.find('td').at(0).children().props().greyedOut
+      ).toEqual(true)
+    }
+    const lastRow = wrapper.find('tbody').find('tr').at(5)
+    expect(
+      lastRow.find('td').at(6).children().props().greyedOut
+    ).toEqual(true)
   })
+  it('should fill the missing cells in the month in the grid with dates from previous & next months', () => {
+    const firstDate = new Date(todayDate.getFullYear(), todayDate.getMonth(), 1)
+    const firstDayIndex = firstDate.getDay()
+    if(firstDayIndex > 0) {
+      const firstRow = wrapper.find('tbody').find('tr').at(0)
+      expect(
+        parseInt(firstRow.find('td').at(0).text())
+      ).toBeGreaterThan(1)
+    }
+    const lastRow = wrapper.find('tbody').find('tr').at(5)
+    expect(
+      parseInt(lastRow.find('td').at(0).text())
+    ).toBeGreaterThan(1)    
+  })
+  it(`should apply the selected style on a newly selected date & isActive style should still be on the original date`, () => {
+    let allTd = wrapper.find('tbody').find('td')
+    let elementToClick = allTd.find(DateDiv).find({selected: false}).children().at(8)
+    elementToClick.simulate('click')
+    allTd = wrapper.find('tbody').find('td')
+    expect(
+      allTd.find({selected: true}).children().at(0).text()
+    ).toEqual(elementToClick.text())
+    expect(
+      allTd.find('td').find({isActive: true}).at(0).text()
+    ).toEqual(todayDate.getDate().toString())
+  })
+
+  // describe(`doesn't throw an error when`, () => {
+  //   it(`saved is clicked`, fail)
+  //   it(`cancel is clicked`, fail)
+  // })
 
 })
 
